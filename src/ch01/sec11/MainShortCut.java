d9781601ccf6a2a4341245257f6e503f91d9ef31
dongukkoo <123609752+dongukkoo@users.noreply.github.com>
2023-02-14 10:54:49 +0900

    1st commit(java)

---
+++ src/ch01/sec11/MainShortCut.java
@@ -0,0 +1,16 @@
+package ch01.sec11;
+
+public class MainShortCut {
+	//main
+	public static void main(String[] args) {
+		// sysout
+//		System.out.println("Hi JAVA");
+		System.out.println("Hi JAVA");
+		// 주석 ctrl * /
+		// 실행 ctrl + f11
+		// 창크기 ctrl + m
+		// 한줄 지우기 ctrl+d
+		//줄 옮기기 ctrl + 방향키위아래
+		// 힌트 얻기 ctrl + space
+	}
+}
